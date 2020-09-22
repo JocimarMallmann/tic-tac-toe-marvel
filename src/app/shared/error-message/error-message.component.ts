de02c44a23ba88b3b555881aa4559e514d68a382
Jocimar Stadler Mallmann <jocimarmallmann@hotmail.com>
2020-09-22 20:57:30 -0300

    Ajustes no css, e implementado a view que mostra a vez do player

--- src/app/shared/error-message/error-message.component.ts
+++ src/app/shared/error-message/error-message.component.ts
@@ -27,7 +27,7 @@ export class ErrorMessageComponent implements OnInit, OnChanges {
       this.render.setStyle(this.divError.nativeElement, 'opacity', '1');
       setTimeout(
         () => this.render.setStyle(
-          this.divError.nativeElement, 'opacity', '0'), 2000
+          this.divError.nativeElement, 'opacity', '0'), 3000
         );
     } else {
       this.render.setStyle(this.divError.nativeElement, 'opacity', '0')
